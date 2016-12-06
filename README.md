41443357b411519973271627b8abc10ac65f8f88
lukem512 <hi@lukemitchell.co>
2016-12-06 16:10:51 +0000

    Add instructions

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Triplicate
 
 Show your photographs simply and beautifully.
+
+<img src="https://i.imgur.com/cGJuJng.jpg" />
+
+### Create your own
+
+To create a Triplicate, clone this repository and run `npm run start` from the command line. You'll find everything you need inside [App.js](src/App.js).
--- src/App.js
+++ src/App.js
@@ -6,14 +6,20 @@ class App extends Component {
   render() {
     return (
       <div className="app">
+        {/* This is the title, displayed at the top of the page. */}
         <div className="header">
           <h1 className="title">Triplicate</h1>
         </div>
+
+        {/* These are links to your photographs. To create a new row,
+          simply add a new Triplicate element. */}
         <Triplicate photos={[
           "https://i.imgur.com/qOzvV95.jpg",
           "https://i.imgur.com/57fNtrZ.jpg",
           "https://i.imgur.com/brm3mBl.jpg",
         ]} />
+
+        {/* This is displayed beneath your photos. */}
         <p className="subtitle">A selection of images on the same theme</p>
       </div>
     );
